d988a4d4d080b2220343813ede332313a2da30d3
Dev-Viradiya <dev.viradiya10@gmail.com>
2026-07-29 17:12:47 +0530

    1312B - Solved

---
+++ 1312B.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    long long t;
+    cin >> t;
+
+    while(t--){
+        long long n; 
+        cin >> n;
+
+        vector<long long> a(n);
+        for(long long i = 0; i < n; i++){
+            cin >> a[i];
+        }
+
+        sort(a.rbegin(), a.rend());
+
+        for(long long i = 0; i < n; i++){
+            cout << a[i] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
